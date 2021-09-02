e880146ed4d7dccc411caa7cc2e30ce35ab429cd
sal <m6567fc@outlook.com>
2021-09-02 18:09:25 +0800

    first commit

---
+++ src/main/kotlin/com/example/nativedemo0/NativeDemo0Application.kt
@@ -0,0 +1,11 @@
+package com.example.nativedemo0
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NativeDemo0Application
+
+fun main(args: Array<String>) {
+	runApplication<NativeDemo0Application>(*args)
+}
---
+++ src/test/kotlin/com/example/nativedemo0/NativeDemo0ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nativedemo0
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NativeDemo0ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
